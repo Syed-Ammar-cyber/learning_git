b1f72e64bd2fa137749162e17682e79ca1c9bc34
Syed Ammar Hussain <syedammarhussaingamer@gmail.com>
2026-02-28 02:23:18 +0530

    multiplication add kr diya

--- src/hello.java
+++ src/hello.java
@@ -5,9 +5,13 @@ public class hello {
     void add() {
         System.out.println("The addition is: " + (a + b));
     }
+    void mult(){
+        System.out.println("The product is: "(a * b));
+    }
 
     public static void main(String[] args) {
         hello obj = new hello();
         obj.add();
+        obj.mult();
     }
 }
