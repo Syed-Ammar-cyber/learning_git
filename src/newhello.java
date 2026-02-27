da1d9c17b7cb6cc699d8499ed713ee4679e67830
Syed Ammar Hussain <syedammarhussaingamer@gmail.com>
2026-02-28 01:13:09 +0530

    everthing new  added

--- src/newhello.java
+++ src/newhello.java
@@ -1,5 +1,6 @@
 public class newhello {
     public static void main(String[] args) {
         System.out.println("new hello world print kra h");
+        System.out.println("new commits ho rhe h ya nahi?");
     }
 }
