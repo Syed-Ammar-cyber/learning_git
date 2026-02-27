8681e48b001f80fb1ca8ce22c0ff735ae3d8e562
Syed Ammar Hussain <syedammarhussaingamer@gmail.com>
2026-02-28 01:14:50 +0530

    three commits

--- src/newhello.java
+++ src/newhello.java
@@ -2,5 +2,6 @@ public class newhello {
     public static void main(String[] args) {
         System.out.println("new hello world print kra h");
         System.out.println("new commits ho rhe h ya nahi?");
+        System.out.println("Three commits?");
     }
 }
